3bc9b3ab01a88fb3c37655a0eed18caf0344dadb
josephadeabah <joseph.adeabah@amalitech.org>
2021-08-12 11:09:28 +0000

    this folder has the connection strings and google passport OAuth 2.0 for google authentication

---
+++ config/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const {Pool} = require('pg')
+const isProduction = process.env.NODE_ENV === 'development'
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+
+const pool = new Pool({
+  connectionString: isProduction ? connectionString : connectionString,
+  // ssl: {
+  //   rejectUnauthorized: false
+  //   }
+})
+console.log(process.env.DATABASE_URL)
+console.log(connectionString);
+console.log(isProduction);
+console.log(pool.ssl)
+module.exports = {
+    port: parseInt(process.env.PORT, 10),
+    pool
+}
